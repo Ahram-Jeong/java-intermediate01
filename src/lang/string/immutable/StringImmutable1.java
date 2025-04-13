b80bc322446d0964462aaaa0d2085adb755bcaeb
Ahram-Jeong <ahram1215@gmail.com>
2025-04-13 16:57:15 +0900

    20250413 - String 클래스_불변 객체

---
+++ src/lang/string/immutable/StringImmutable1.java
@@ -0,0 +1,9 @@
+package lang.string.immutable;
+
+public class StringImmutable1 {
+    public static void main(String[] args) {
+        String str = "Xdinary";
+        str.concat(" Heroes");
+        System.out.println("str = " + str); // Xdinary
+    }
+}
