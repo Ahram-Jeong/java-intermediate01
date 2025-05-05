19c735595bfa43d1b2d0c1682d6d32ea47be8964
Ahram-Jeong <ahram1215@gmail.com>
2025-05-05 13:42:51 +0900

    20250505 - 열거형 ENUM : 타입 안전 열거형 패턴 2

---
+++ src/enumeration/ex2/ClassGradeEx2_2.java
@@ -0,0 +1,14 @@
+package enumeration.ex2;
+
+public class ClassGradeEx2_2 {
+    public static void main(String[] args) {
+        int price = 10000;
+
+        DiscountService discountService = new DiscountService();
+        /*
+        ClassGrade newClassGrade = new ClassGrade(); // 새롭게 생성 -> ClassGrade 클래스에 private 생성자를 추가해서 방지
+        int result = discountService.discount(newClassGrade, price);
+        System.out.println("newClassGrade 등급의 할인 금액 = " + result);
+        */
+    }
+}
